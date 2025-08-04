2f8744b2bdef67adf46d338c897ec38288fd44e2
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-04 10:42:08 -0400

    Initial Commit

---
+++ weSplit/weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitApp.swift
+//  weSplit
+//
+//  Created by Qayyax on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weSplitTests/weSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitTests.swift
+//  weSplitTests
+//
+//  Created by Qayyax on 8/4/25.
+//
+
+import Testing
+@testable import weSplit
+
+struct weSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
