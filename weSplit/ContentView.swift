69db80687a9581ba934ef14e03a6e4fc59cc542b
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-05 07:24:57 -0400

    almost done with chapter

--- weSplit/ContentView.swift
+++ weSplit/ContentView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View  {
+  @State private var checkAmount = 0.0
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 20
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
   var body: some View {
-    Form{
-      ForEach(0..<100) { number in
-        Text("Row \(number)")
-      }
-    }
+    Text("Hello, world!")
   }
 }
 
